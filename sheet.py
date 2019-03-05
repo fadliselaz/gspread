4174ec5efa2839c2627d68d4b824ca6760a67519
fadliselaz <fadliselaz@Pratiwis-MacBook.local>
2019-03-06 00:01:35 +0700

    jadikan method

--- sheet.py
+++ sheet.py
@@ -1,8 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
 credentials = ServiceAccountCredentials.from_json_keyfile_name('test.json', scope)
 
 #menjadikan variable gc
@@ -11,4 +9,3 @@ gc = gspread.authorize(credentials)
 # work sheet yang dipakai
 wks = gc.open("sheet").sheet1
 
-print(wks.get_all_records())
